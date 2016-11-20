81e105d6810dfcb452459e2b06470fe348d6d1b2
pzl <dan@panzarel.la>
2016-11-20 18:32:32 -0500

    break up calc flux into fns

--- bootstrap.py
+++ bootstrap.py
@@ -114,28 +114,30 @@ def get_weighted_climo(date):
 	lats = climo.variables['lat'][climo_lat_indices]
 	lat_rads = lats * (math.atan(1.0)/45.0)
 	lat_cos = numpy.cos(lat_rads)
-	weighted_mean_climo = numpy.average( filtered_climo, axis=None,  weights=lat_cos)
+	return numpy.average( filtered_climo, axis=None,  weights=lat_cos)
 
-	log("accessing merid wind and temp data at: %s and %s","/langlab_rit/hattard/merra2/%s" % (season % ("v",),),"/langlab_rit/hattard/merra2/%s" % (season % ("t",),))
 
+def open_vt_files(season):
 	meridional_wind = netCDF4.Dataset("/langlab_rit/hattard/merra2/%s" % (season % ("v",),))
 	temp = netCDF4.Dataset("/langlab_rit/hattard/merra2/%s" % (season % ("t",),))
 
+	return (meridional_wind,temp)
 
+def get_vt_indices(date, mw, temp):
 	# V time index is YYYYMMDDHH, we are going to look for hours 0,6,12,18
 	time_fmt = "%04d%02d%02d%%02d" % (date.year,date.month,date.day)
 	log("Looking for time_FMT %s",time_fmt)
 	vt_time_indices = numpy.where(
 						numpy.logical_and(
-							meridional_wind.variables['time'][:] >= int(time_fmt % (0,)),
-							meridional_wind.variables['time'][:] <= int(time_fmt % (18,))
+							mw.variables['time'][:] >= int(time_fmt % (0,)),
+							mw.variables['time'][:] <= int(time_fmt % (18,))
 						)
 					)[0]
-	vt_lev = numpy.where( meridional_wind.variables['lev'][:] == 100 )[0][0]
+	vt_lev = numpy.where( mw.variables['lev'][:] == 100 )[0][0]
 	vt_lat_indices = numpy.where(
 						numpy.logical_and(
-							meridional_wind.variables['lat'][:] >= 45,
-							meridional_wind.variables['lat'][:] <= 75
+							mw.variables['lat'][:] >= 45,
+							mw.variables['lat'][:] <= 75
 						)
 					)[0]
 
@@ -143,11 +145,23 @@ def get_weighted_climo(date):
 	log("vt_lev: %s",vt_lev)
 	log("vt_lat_indices: %s",vt_lat_indices)
 
+	return (vt_time_indices,vt_lev,vt_lat_indices)
 
+def get_vt_data(mw, temp, tm, lev, lat, date):
 	# Read V and T data at the times, lev 100, lat range, and all lons
-	v = meridional_wind.variables['v'][vt_time_indices, vt_lev, vt_lat_indices, :]
-	t = temp.variables['t'][vt_time_indices, vt_lev, vt_lat_indices, :]
+	try:
+		v = mw.variables['v'][tm, lev, lat, :]
+		t = temp.variables['t'][tm, lev, lat, :]
+	except ValueError:
+		print("Got some sort of error (below) when reading v and t data")
+		print("  date: %s" % (date,))
+		print("  time indices: %s" % (vt_time_indices,))
+		print("  vt lev index: %s" % (vt_lev,))
+		print("  vt lat indices: %s" % (vt_lat_indices,))
+		raise
+	return (v,t)
 
+def make_vt_anomaly(v,t):
 	log("performing v and t averages")
 	# averaged across times
 	v_time_avg = numpy.average(v,0)
@@ -167,7 +181,20 @@ def get_weighted_climo(date):
 	vt_zon_anomaly = v_zon_anomaly * t_zon_anomaly
 
 	# average across lons again
-	vt_zon_anomaly_lon_avg = numpy.average(vt_zon_anomaly,1)
+	return numpy.average(vt_zon_anomaly,1)
+
+def get_weighted_vt(date):
+	season = find_season(date) # get season in file path format, waiting for t/v replacement
+
+	log("accessing merid wind and temp data at: %s and %s","/langlab_rit/hattard/merra2/%s" % (season % ("v",),),"/langlab_rit/hattard/merra2/%s" % (season % ("t",),))
+
+	meridional_wind, temp = open_vt_files(season)
+
+	vt_time_indices, vt_lev, vt_lat_indices = get_vt_indices(date,meridional_wind,temp)
+
+	v,t = get_vt_data(meridional_wind, temp, vt_time_indices, vt_lev, vt_lat_indices, date)
+
+	vt_zon_anomaly_lon_avg = make_vt_anomaly(v,t)
 
 	log("Getting weighted zon anom")
 
@@ -175,12 +202,17 @@ def get_weighted_climo(date):
 	vt_lats = meridional_wind.variables['lat'][vt_lat_indices]
 	vt_lats_rads = vt_lats * (math.atan(1.0)/45.0)
 	vt_lats_cos = numpy.cos(vt_lats_rads)
-	weighted_mean_vt_zon_anomaly = numpy.average( vt_zon_anomaly_lon_avg, axis=None, weights=vt_lats_cos )
+	return numpy.average( vt_zon_anomaly_lon_avg, axis=None, weights=vt_lats_cos )
+
+def calc_flux(date):
+	log("calculating flux. date: %s",date)
+
+	weighted_mean_climo = get_weighted_climo(date)
+	weighted_mean_vt_zon_anomaly = get_weighted_vt(date)
 
 	heat_flux_anom = weighted_mean_vt_zon_anomaly - weighted_mean_climo
 
 	log("for date %s, got value %s",date,heat_flux_anom)
-
 	return heat_flux_anom
 
 
