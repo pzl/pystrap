231ec73857c90bc5670fe7ca7b7b762eed8003a4
pzl <dan@panzarel.la>
2016-12-03 13:12:47 -0500

    more correct datelist maker with valid days

--- make_dates.py
+++ make_dates.py
@@ -1,15 +1,16 @@
 import random
+from calendar import monthrange
 
 dlist = []
 
 for x in range(100):
 	year = random.randint(1980,2015)
-	month = random.randint(1,12)
-	day = random.randint(1,30)
+	month = random.choice([1,2,3,4,10,11,12])
+	day = random.randint(1,monthrange(year,month)[1])
 	hour = random.choice([0,6,12,18])
 
 	dlist.append("%04d%02d%02d%02d" % (year,month,day,hour))
 
 with open("dates.txt","w") as f:
 	for d in dlist:
-		f.write("%s\n" % (d,))
+		f.write("a b %s\n" % (d,))
